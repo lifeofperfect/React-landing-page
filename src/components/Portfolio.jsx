d41ae70136b68eecab2ce15c40c4e3ba68c64936
lifeofperfect <perfect.igbadumhe@gmail.com>
2020-07-22 01:47:30 +0100

    finished landing page

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+export default ({ portfolioLinks }) => {
+  return (
+    <div>
+      <section className="page-section bg-light" id="portfolio">
+        <div className="container">
+          <div className="text-center">
+            <h2 className="section-heading text-uppercase">Portfolio</h2>
+            <h3 className="section-subheading text-muted">
+              Lorem ipsum dolor sit amet consectetur.
+            </h3>
+          </div>
+          <div className="row">
+            {portfolioLinks.map(({ title, caption }, index) => (
+              <div className="col-lg-4 col-sm-6 mb-4">
+                <div className="portfolio-item">
+                  <a
+                    className="portfolio-link"
+                    data-toggle="modal"
+                    href="#portfolioModal1"
+                  >
+                    <div className="portfolio-hover">
+                      <div className="portfolio-hover-content">
+                        <i className="fa fa-plus fa-3x"></i>
+                      </div>
+                    </div>
+                    <img
+                      className="img-fluid"
+                      src="http://lorempixel.com/400/200/"
+                      alt=""
+                    />
+                  </a>
+                  <div className="portfolio-caption">
+                    <div className="portfolio-caption-heading">{title}</div>
+                    <div className="portfolio-caption-subheading text-muted">
+                      {caption}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
